43611066e05cce0fcb75b584f331814e566b36f8
bschamel <schamelb4@gmail.com>
2014-06-25 16:54:31 -0500

    Start

---
+++ NumberMind185/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NumberMind185
+{
+	public class pair<T1, T2>
+	{
+		public T1 v1;
+		public T2 v2;
+	}
+	public class NumberMind
+	{
+		public List<pair<byte[], int>> guesses = new List<pair<byte[],int>>();
+
+		public List<List<byte[]>> possibles = new List<List<byte[]>>();
+
+	}
+	class Program
+	{
+		static void Main(string[] args)
+		{
+		}
+	}
+}
